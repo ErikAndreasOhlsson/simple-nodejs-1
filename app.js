6c942699776f7d114cac99616d60765e6aed1ab0
Andreas Ohlsson <erik.andreas.ohlsson@live.se>
2021-12-20 23:03:52 +0100

    Basic node backend

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const staticBasePath = './www';
+
+const staticServe = function (req, res) {
+    let resolvedBase = path.resolve(staticBasePath);
+    let safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
+    let fileLoc = path.join(resolvedBase, safeSuffix);
+    fs.readFile(fileLoc, function (err, data) {
+        if (err) {
+            res.writeHead(404, 'Not Found');
+            res.write('404: File Not Found!');
+            return res.end();
+        }
+        res.statusCode = 200;
+        res.write(data);
+        return res.end();
+    });
+};
+
+const httpServer = http.createServer(staticServe);
+
+httpServer.listen(8080, () => {
+    console.log("server listening on port 8080");
+});
